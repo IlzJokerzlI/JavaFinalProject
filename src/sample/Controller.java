525071bd59676ce08d4b3c5c45125b21ef4074ce
IlzJokerzlI <arvin.lee@binus.ac.id>
2020-06-02 13:54:28 +0700

    First Commit

---
+++ src/sample/Controller.java
@@ -0,0 +1,30 @@
+package sample;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.*;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+import java.awt.*;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+
+    public void setPathField() {
+
+    }
+
+    public void loadFiles() {
+    }
+}
