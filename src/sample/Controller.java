6a608b1af852ca3c3ea1bf708751082dffaca45a
IlzJokerzlI <arvin.lee@binus.ac.id>
2020-06-07 16:22:18 +0700

    Changing string type to File object type storing

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -11,12 +11,14 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.*;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import net.lingala.zip4j.exception.ZipException;
 import org.w3c.dom.ls.LSOutput;
 
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.beans.EventHandler;
 import java.io.File;
+import java.io.FileFilter;
 import java.io.FilenameFilter;
 import java.net.URL;
 import java.nio.file.Path;
@@ -25,6 +27,8 @@ import java.util.*;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
+
+import java.util.Map;
 
 public class Controller implements Initializable{
 
@@ -48,38 +52,61 @@ public class Controller implements Initializable{
     private final String defaultPath = "/home";
     private String currentPath = this.defaultPath;
 
+    Map<String, File> tempChildrenPaths = new LinkedHashMap<String, File>();
+    Map<String, File> selectedChildrenPaths = new LinkedHashMap<String, File>();
+
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         displayFiles();
         this.explorerListView.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
-
-        this.explorerListView.setOnMousePressed(event -> openDir(event));
-
+        this.selectedListView.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
     }
 
-    public void displayFiles() {
-        File file = new File(this.currentPath);
-        if (file.exists()) {
-            List<String> tempList = new ArrayList<String>(Arrays.stream(file.listFiles()).filter((f) -> !f.isHidden()).map((f) -> f.getName()).collect(Collectors.toList())); //Scan files
-            tempList.add(0,".."); //Add back option
+//    public void displayFiles() {
+//        File file = new File(this.currentPath);
+//        if (file.exists()) {
+//            List<String> tempList = new ArrayList<String>(Arrays.stream(file.listFiles()).filter((f) -> !f.isHidden()).map((f) -> f.getName()).collect(Collectors.toList())); //Scan files
+//            tempList.add(0,".."); //Add back option
+//
+//            this.explorerListView.getItems().clear();
+//            this.explorerListView.getItems().addAll(tempList);
+//            this.pathField.setText(file.getPath());
+//        }
+//    }
 
-            this.explorerListView.getItems().clear();
-            this.explorerListView.getItems().addAll(tempList);
-            this.pathField.setText(file.getPath());
+    public void displayFiles() {
+        File currentFile = new File(this.currentPath);
+
+        this.tempChildrenPaths.clear();
+        this.explorerListView.getItems().clear();
+
+        if (currentFile.exists()) {
+            this.tempChildrenPaths.put("..", currentFile.getParentFile()); //Adds back option
+
+            for (File childFile : Objects.requireNonNull(currentFile.listFiles(new FileFilter() {
+                @Override
+                public boolean accept(File file) {
+                    return file.exists() && !file.isHidden();
+                }
+            }))) {
+                this.tempChildrenPaths.put(childFile.getName(), childFile);
+            }
+
+            this.explorerListView.getItems().addAll(this.tempChildrenPaths.keySet()); //Sets the file list in list view
+            this.pathField.setText(currentFile.getPath()); //Sets the path field to the current path
         }
     }
 
     public void clearPath() {
         this.currentPath = this.defaultPath;
         displayFiles();
-        this.pathField.setText(this.currentPath);
     }
 
     public void searchPath() {
-        File tempFile = new File(this.pathField.getText());
+        File searchedFile = new File(this.pathField.getText());
 
-        if (tempFile.exists() && tempFile.isDirectory()) {
+        if (searchedFile.exists() && searchedFile.isDirectory()) {
             this.currentPath = this.pathField.getText();
             displayFiles();
         } else {
@@ -90,18 +117,16 @@ public class Controller implements Initializable{
     public void openDir(MouseEvent event) {
         if (event.isPrimaryButtonDown() && event.getClickCount() == 2) {
             String selectedItem = this.explorerListView.getSelectionModel().getSelectedItem();
-            File tempFile;
 
             //Setting file path
             if (selectedItem.equals("..")) {
-                tempFile = new File(this.currentPath);
-                this.currentPath = tempFile.getParent();
+                this.currentPath = this.tempChildrenPaths.get("..").getPath();
             } else {
-                tempFile = new File(currentPath + "/" + selectedItem);
-                if (tempFile.exists() && tempFile.isDirectory()) {
-                    this.currentPath = tempFile.getPath();
+                if ( this.tempChildrenPaths.get(selectedItem).exists() && this.tempChildrenPaths.get(selectedItem).isDirectory()) {
+                    this.currentPath = this.tempChildrenPaths.get(selectedItem).getPath();
                 } else {
-                    System.out.println(tempFile.getName());
+                    System.out.println(this.tempChildrenPaths.get(selectedItem).getName());
+                    return;
                 }
             }
             displayFiles();
@@ -110,17 +135,44 @@ public class Controller implements Initializable{
 
     public void addFile() {
         List<String> selectedItems = this.explorerListView.getSelectionModel().getSelectedItems();
-        selectedItems = selectedItems.stream().filter((f) -> !selectedListView.getItems().contains(f) && !f.contains("..")).collect(Collectors.toList());
-        if (!selectedItems.isEmpty()) {
-            selectedListView.getItems().addAll(selectedItems);
+
+        this.selectedListView.getItems().clear();
+
+        for (String fileName : selectedItems) {
+            if (!fileName.isEmpty() && !this.selectedChildrenPaths.containsKey(fileName) && !fileName.equals("..")) {
+                this.selectedChildrenPaths.put(fileName, this.tempChildrenPaths.get(fileName));
+            }
         }
+
+        this.selectedListView.getItems().addAll(this.selectedChildrenPaths.keySet());
     }
 
     public void removeFile() {
-
+        this.selectedListView.getItems().removeAll(this.selectedListView.getSelectionModel().getSelectedItems());
+        this.selectedChildrenPaths.remove(this.selectedListView.getSelectionModel().getSelectedItems());
     }
 
     public void clearFile() {
+        this.selectedListView.getItems().clear();
+        this.selectedChildrenPaths.clear();
+    }
 
+    public void setEncryptStatus() {
+        if (this.encryptCheckBox.isSelected()) {
+            this.passwordField.setDisable(false);
+        } else {
+            this.passwordField.clear();
+            this.passwordField.setDisable(true);
+        }
+    }
+
+    public void execute() {
+        if (!this.selectedChildrenPaths.isEmpty()) {
+            try {
+                new ZipFile("/home/arvin").addFiles(new ArrayList<File>(this.tempChildrenPaths.values()));
+            } catch (ZipException ex) {
+                System.out.println(ex.getMessage());
+            }
+        }
     }
 }
